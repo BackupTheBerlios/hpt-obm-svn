57c48ef514fbb2f16fee040119df544150b6e337
ductk <ductk@d2254c44-35e1-0310-9233-a49555f92340>
2004-08-30 08:39:30 +0000

    fix header for LUSH theme

--- trunk/hptgo/themes/lush/header.php
+++ trunk/hptgo/themes/lush/header.php
@@ -14,7 +14,7 @@ header('Content-Type: text/html; charset='.$charset);
 <body marginwidth="0" marginheight="0" leftmargin="0" topmargin="0">
 <table border="0" cellpadding="0" cellspacing="0" width="100%" height="23">
 <tr>
-	<td class="HeaderBar" align="left" nowrap>&nbsp;&nbsp;<b><?php echo htmlspecialchars($_SESSION['GO_SESSION']['name']); ?></b>td>
+	<td class="HeaderBar" align="left" nowrap>&nbsp;&nbsp;<b><?php echo htmlspecialchars($_SESSION['GO_SESSION']['name']); ?></b><td>
 	<td class="HeaderBar" align="right" width="100%">
 
 			<a class="HeaderBar" href="<?php echo $GO_CONFIG->host; ?>configuration/" target="main">
