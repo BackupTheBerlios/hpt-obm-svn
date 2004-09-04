e71f1afa384a9f61a497133dd84188db05c7dd1c
pclouds <pclouds@d2254c44-35e1-0310-9233-a49555f92340>
2004-09-04 07:55:49 +0000

    Removed old opentts system.

--- trunk/hptgo/modules/opentts/themes/Aqua/buttons.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function draw_button($url,$title,$selected=FALSE){
-	global $name;
-	if ($selected) $class="<font color=red>"; else $class="<font color=white>";
-	return "<td><a href=\"$url\">$class$title</font></a></td>";
-}
-?>
