d3e3f640b71e2e91d4c3f6e82440c09a9406a121
pclouds <pclouds@d2254c44-35e1-0310-9233-a49555f92340>
2004-11-01 04:19:30 +0000

    Removed squirrelmail and messages from trunk.

--- trunk/hptgo/modules/messages/sql/messages.uninstall.sql
+++
@@ -1,3 +0,0 @@
-DROP TABLE IF EXISTS `messages_messages`;
-DROP TABLE IF EXISTS `messages_new`;
-DROP TABLE IF EXISTS `messages_users`;
--- trunk/hptgo/modules/squirrelmail/sql/squirrelmail.uninstall.sql
+++
@@ -1,4 +0,0 @@
-DROP TABLE IF EXISTS `sqemAccounts`;
-DROP TABLE IF EXISTS `sqemFilters`;
-DROP TABLE IF EXISTS `sqemFolders`;
-DROP TABLE IF EXISTS `sqem_settings`;
